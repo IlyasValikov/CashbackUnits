076422c3e4e1447057670cf57e6259f2eada371a
Ilya Valikov <valikov.skeleton1977@yandex.ru>
2023-06-17 19:02:44 +0300

    Junit tests

--- src/test/java/ru/netology/CashbackHackServiceTest.java
+++ src/test/java/ru/netology/CashbackHackServiceTest.java
@@ -1,7 +1,7 @@
 package ru.netology;
 
-import org.testng.annotations.Test;
-import static org.testng.Assert.assertEquals;
+import org.junit.Test;
+import static org.junit.Assert.assertEquals;
 public class CashbackHackServiceTest {
      @Test
     void shouldCalculateCashbackIfAmountLess1000() {
